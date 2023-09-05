b4961c6b38e557da425bc5b594983aca4d91efbd
kotobukid <kotobuki@daqua.jp>
2023-09-05 13:44:21 +0900

    socket.ioのサンプルを導入

--- bin/www.ts
+++ bin/www.ts
@@ -23,11 +23,28 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
+
+import { Server } from "socket.io";
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port);
+// server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
